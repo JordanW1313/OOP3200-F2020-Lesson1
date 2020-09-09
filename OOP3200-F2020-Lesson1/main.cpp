189df0ace68c14c51b0411cafc091e2f14d0ce0b
Jordan Wriker <wrikerjordan@hotmail.com>
2020-09-09 11:38:22 -0400

    Added welcomeFunction

--- OOP3200-F2020-Lesson1/main.cpp
+++ OOP3200-F2020-Lesson1/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+static void welcomeFunction()
+{
+    std::cout << "Welcome to the Course" << std::endl;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
+
+    welcomeFunction();
 }
